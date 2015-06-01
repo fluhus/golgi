ffd707a6c346e3206b8884b42c921ece65c46c63
Amit Lavon <amitlavon@cs.huji.ac.il>
2015-06-01 21:07:15 +0300

    bwt - added comments and tests

--- src/bwt/bwt.go
+++ src/bwt/bwt.go
@@ -3,9 +3,11 @@ package bwt
 
 import (
 	"bytes"
-	"fmt"
 	"sort"
 )
+
+
+// ----- TRANSFORM -------------------------------------------------------------
 
 // Returns a BW-transform of the given sequence, without changing it. Adds a $
 // sign as a terminating character.
@@ -39,7 +41,7 @@ func (b *bwSorter) sort() *bwSorter {
 	return b
 }
 
-// Sorting functions.
+// Sorting interface functions.
 func (b *bwSorter) Len() int {
 	return len(b.perm)
 }
@@ -60,15 +62,20 @@ func (b *bwSorter) transform() []byte {
 	return arr
 }
 
+
+// ----- RANK INDEX ------------------------------------------------------------
+
 // Indexes the ranks of characters in a BW-transformed sequence.
 type rankIndex struct {
+	str   []byte       // Input sequence.
 	chars map[byte]int // From char to index in rank.
 	ranks [][]int      // Ranks[i] is ranks at i*jump including.
 	jump  int          // How often to save ranks.
 }
 
+// Creates an index for the given BW-transformed sequence.
 func newRankIndex(str []byte, jump int) *rankIndex {
-	result := &rankIndex{nil, nil, jump}
+	result := &rankIndex{str, nil, nil, jump}
 
 	// Create a map of characters to index in the rank arrays.
 	chars := make(map[byte]int)
@@ -87,7 +94,7 @@ func newRankIndex(str []byte, jump int) *rankIndex {
 
 	// Create ranks array (+-1 because the first out of each jump gets
 	// an array).
-	ranks := make([][]int, (len(str)-1)/jump+1)
+	ranks := make([][]int, (len(str)-1) / jump+1)
 
 	// Count ranks in sequence.
 	rank := make([]int, len(chars))
@@ -96,10 +103,10 @@ func newRankIndex(str []byte, jump int) *rankIndex {
 		rank[chars[b]]++
 
 		// Copy array if hit a checkpoint.
-		if i%jump == 0 {
+		if i % jump == 0 {
 			cp := make([]int, len(rank))
 			copy(cp, rank)
-			ranks[i/jump] = cp
+			ranks[i / jump] = cp
 		}
 	}
 
@@ -108,34 +115,12 @@ func newRankIndex(str []byte, jump int) *rankIndex {
 	return result
 }
 
-func (r *rankIndex) String() string {
-	buf := bytes.NewBuffer(nil)
-
-	fmt.Fprint(buf, "chars: ")
-	for b, i := range r.chars {
-		fmt.Fprintf(buf, "%d-%c ", i, b)
-	}
-	fmt.Fprintln(buf)
-
-	fmt.Fprintln(buf, "jump:", r.jump)
-
-	fmt.Fprintln(buf, "ranks:")
-	for _, rank := range r.ranks {
-		for _, r := range rank {
-			fmt.Fprintf(buf, "%d\t", r)
-		}
-		fmt.Fprintln(buf)
-	}
-
-	return buf.String()
-}
-
 func (r *rankIndex) rankOf(char byte, at int, str []byte) int {
 	// Ranks at the closest rank array.
-	result := r.ranks[at/r.jump][r.chars[char]]
+	result := r.ranks[at / r.jump][r.chars[char]]
 
 	// Count up to 'at'.
-	for i := at/r.jump*r.jump + 1; i <= at; i++ {
+	for i := at / r.jump*r.jump + 1; i <= at; i++ {
 		if str[i] == char {
 			result++
 		}
@@ -143,3 +128,4 @@ func (r *rankIndex) rankOf(char byte, at int, str []byte) int {
 
 	return result
 }
+
--- src/bwt/bwt_test.go
+++ src/bwt/bwt_test.go
@@ -2,9 +2,18 @@ package bwt
 
 import (
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestTransform(t *testing.T) {
-	
+func TestTransform_word(t *testing.T) {
+	assert := assert.New(t)
+	out := string(Transform([]byte("abracadabra")))
+	assert.Equal("ard$rcaaaabb", out)
+}
+
+func TestTransform_empty(t *testing.T) {
+	assert := assert.New(t)
+	out := string(Transform(nil))
+	assert.Equal("$", out)
 }
 
